061008b3051e09c9fd869f82108533356edc4331
Shivansh Agarwal <shivanshag1998@gmail.com>
2020-04-10 08:41:49 +0530

    Maven Project ready to push to github

---
+++ ProjectMVN/Assignment1/src/main/java/com/sapientAssignments/App.java
@@ -0,0 +1,10 @@
+package com.sapientAssignments;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Maven Project Successful Build !" );
+    }
+}
