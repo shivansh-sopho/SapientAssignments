39d36008a1714e47e69b80a2f157207b575e0766
Shivansh Agarwal <shivanshag1998@gmail.com>
2020-05-08 14:35:39 +0530

    Added Assignment 5

---
+++ Assignment5/src/Quiz.java
@@ -0,0 +1,23 @@
+package assignment5;
+
+public class Quiz {
+	
+	private String question;
+	private String opt1;
+	private String opt2;
+	private String opt3;
+	private String opt4;
+	private String correctOpt;
+	
+	public Quiz(String q , String o1, String o2 , String o3 , String o4 , String co) {
+		this.question = q;
+		this.opt1 = o1;
+		this.opt2 = o2;
+		this.opt3 = o3;
+		this.opt4 = o4;
+		this.correctOpt = co;
+	}
+	
+	
+
+}
